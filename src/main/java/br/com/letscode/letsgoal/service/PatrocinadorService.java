a775589146634b0fda7bbaa5573dff78a108ba25
Vinicius Levy <viniciusmlevy@gmail.com>
2022-07-13 20:49:04 -0300

    .

--- src/main/java/br/com/letscode/letsgoal/service/PatrocinadorService.java
+++ src/main/java/br/com/letscode/letsgoal/service/PatrocinadorService.java
@@ -1,2 +1,16 @@
-package br.com.letscode.letsgoal.service;public class PatrocinadorService {
-}
+//package br.com.letscode.letsgoal.service;
+//
+//import br.com.letscode.letsgoal.model.Patrocinador;
+//import lombok.AllArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+////@Service
+////@AllArgsConstructor
+////public class PatrocinadorService {
+////
+////    final PatrocinadorRepository patrocinadorRepository;
+////
+////    public Patrocinador savePatrocinador(Patrocinador patrocinador) {
+////        return patrocinadorRepository.save(patrocinador);
+////    }
+//}
