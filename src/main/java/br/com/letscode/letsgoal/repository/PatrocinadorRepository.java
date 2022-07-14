41c8c04839507e1a7ab3d7e2c8e7904ff3a2f8bb
Yago <yagogomessantos@gmail.com>
2022-07-13 23:32:49 -0300

    Iniciando o projeto

--- src/main/java/br/com/letscode/letsgoal/repository/PatrocinadorRepository.java
+++ src/main/java/br/com/letscode/letsgoal/repository/PatrocinadorRepository.java
@@ -1,8 +1,9 @@
 package br.com.letscode.letsgoal.repository;
 
 import br.com.letscode.letsgoal.model.Patrocinador;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PatrocinadorRepository extends CrudRepository<Patrocinador, Long> {
-
+@Repository
+public interface PatrocinadorRepository extends JpaRepository<Patrocinador, Long> {
 }
