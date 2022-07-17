940b976117666fae15c372c75b0987668bc0e283
Rafael Rocha <rafael.rcavalcanti@dataprev.gov.br>
2022-07-17 15:09:59 -0300

    add load test

--- src/main/java/br/com/letscode/letsgoal/security/ApplicationSecurityConfig.java
+++ src/main/java/br/com/letscode/letsgoal/security/ApplicationSecurityConfig.java
@@ -48,12 +48,12 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // se API sem estado e proteção CSRF fizer sentido, remover!
             .authorizeRequests()
+                .antMatchers("/formacoes").permitAll()
                 .antMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
                 .antMatchers("/patrocinadores").hasRole("USER")
-                .anyRequest().authenticated() 
+                .anyRequest().authenticated()
             .and()
                 .formLogin()
             .and()
