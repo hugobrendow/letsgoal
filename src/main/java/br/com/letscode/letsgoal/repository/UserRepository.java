720d5b46dcccfad800979d490b09c14dc8be86ed
jacksonobjective <jackson.braga@objective.com.br>
2022-07-20 19:36:06 -0300

    Database Authentication

--- src/main/java/br/com/letscode/letsgoal/repository/UserRepository.java
+++ src/main/java/br/com/letscode/letsgoal/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package br.com.letscode.letsgoal.repository;
 
 import br.com.letscode.letsgoal.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
